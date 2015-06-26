247cb87c7c28fd490a8b34e0664f44970aa19a6a
Prashant <prashant.barca@gmail.com>
2015-06-26 13:14:09 +0530

    Try to make variables more meaningful

--- hodor.rb
+++ hodor.rb
@@ -1,28 +1,31 @@
 require 'rest_client'
 while 1
-    x = ""
+    offset = ""
     File.open "offset","r" do |a|
-        x = x+ a.read
+        offset = offset+ a.read
     end
-    response = RestClient.get "https://api.telegram.org/#{ENV['TELEGRAM_API']}/getUpdates", {:params => {"offset" => x.to_i+1}}
+    response = RestClient.get "https://api.telegram.org/#{ENV['TELEGRAM_API']}/getUpdates", {:params => {"offset" => offset.to_i+1}}
     updates = JSON.parse(response)["result"]
     updates.each do |update|
-        u = update
-        if u["message"]["text"] && u["message"]["text"].length<50
-            #puts u["message"]["text"]
-            length = u["message"]["text"].split(' ').length
+        if update["message"]["text"] 
+            if update["message"]["text"].length < 50
+                #puts u["message"]["text"]
+                length = update["message"]["text"].split(' ').length
+            else 
+                length = 1
+            end
         else
             length = 1
         end
+
         message =""
         length.times do 
             message = message + "Hodor "
         end
-
-        xyz= RestClient.get "https://api.telegram.org/#{ENV['TELEGRAM_API']}/sendMessage" ,{:params => {"chat_id"=> u["message"]["chat"]["id"], "text"=> message}}
-        if u["update_id"].to_i>x.to_i
+        xyz= RestClient.get "https://api.telegram.org/#{ENV['TELEGRAM_API']}/sendMessage" ,{:params => {"chat_id"=> update["message"]["chat"]["id"], "text"=> message}}
+        if update["update_id"].to_i>offset.to_i
             File.open "offset","w" do |file|
-                file.write u["update_id"]
+                file.write update["update_id"]
             end
         end
     end
